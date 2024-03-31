1f87cb1eae4271a934c8dafe7d55597dcc19c937
Animish2002 <animishchopade123@gmail.com>
2024-03-31 16:39:16 +0530

    login form react functional

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginForm from './LoginForm';
+import LoginForm from './Components/Login/LoginForm';
 
 const App = () => {
   return (
